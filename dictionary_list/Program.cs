f83d5aa3b5c5ebc317ff60a79f0d32e38ba662ad
ayhan 2855 <Ayhan28.ak@outlook.com.tr>
2021-10-08 22:37:16 +0300

    kolleksiyonlar_soru_1

---
+++ dictionary_list/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionary_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //System.Collection.Generic
+            Dictionary<int,string> users = new Dictionary<int, string>();
+            users.Add(10,"Ayhan Karaman");
+            users.Add(12,"İlker Yiğitalp");
+            users.Add(18,"Selçuk Bakış");
+            users.Add(20,"Bülent Demir");
+
+            //Dizinin elemanlarına erişim
+            Console.WriteLine(users[12]);
+            foreach (var item in users)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Count
+            Console.WriteLine("***** Count *****");
+            Console.WriteLine(users.Count);
+
+            //Contains
+            Console.WriteLine("***** Contains *****");
+            Console.WriteLine(users.ContainsKey(12));
+            Console.WriteLine(users.ContainsValue("Ahmet Karaman"));
+
+            // Remove
+            Console.WriteLine("***** Remove *****");
+            users.Remove(12);
+            foreach (var item in users)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Keys
+            Console.WriteLine("***** Keys *****");
+              foreach (var item in users)
+            {
+                Console.WriteLine(item.Key);
+            }
+
+            //Values
+            Console.WriteLine("***** Values *****");
+              foreach (var item in users)
+            {
+                Console.WriteLine(item.Value);
+            } 
+   
+        }
+    }
+}
---
+++ lessons/collection_question_1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+
+namespace collection_question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ArrayList wandList = new ArrayList();
+            ArrayList notWandList = new ArrayList();
+            
+            
+            //Console.Write("Lütfen girmek istediğiniz sayı adedini giriniz : ");Convert.ToInt32(Console.ReadLine());
+            int n = 20;
+              
+              
+             for (int i =1 ; i <=n; i++)
+            {
+                 Console.WriteLine($"Lütfen {i}. sayıyı giriniz.");
+                 string  value = Console.ReadLine();
+                  int number;
+                 bool result = int.TryParse(value, out number);
+                 if(!result){
+                      Console.WriteLine("Lütfen Sayısal bir değer giriniz");
+                      break;
+                 }
+                 else if(number <=0){
+                      Console.WriteLine("Lütfen pozitif bir sayı giriniz");
+                       break;
+                 }
+                 else{
+                 int counter = 0;
+                  for (int j = 2; j < number; j++)
+             {
+                  if(number % j == 0){counter++;}
+             }
+
+              if(counter == 0){
+               wandList.Add(number);
+              }else{
+                  notWandList.Add(number);
+              }
+            }
+            }  
+           
+           
+           Console.WriteLine("\n******* Asal Sayılar *******");
+           wandList.Sort();
+           wandList.Reverse();
+           foreach (var item in wandList)
+           {
+               Console.WriteLine(item);
+           }
+
+
+          
+           Console.WriteLine("\n******** Asal Olmayan Sayılar ******");
+           notWandList.Sort();
+           notWandList.Reverse();
+           foreach (var item in notWandList)
+           {
+                Console.WriteLine(item);
+           }
+
+           // Asal sayıların toplam eleman sayısı ve ortalaması
+            Console.WriteLine("\nAsal sayıların toplam eleman sayısı : "+wandList.Count);
+            int wandTotal = 0;
+            double wandAverage = 0;
+           for (int i = 0; i < wandList.Count; i++)
+           {
+                wandTotal += Convert.ToInt32(wandList[i]);
+                wandAverage = (double)wandTotal/(wandList.Count);
+           }
+           Console.WriteLine("Asal sayıların ortalama sayısı : "+ wandAverage);
+
+
+
+             // Asal olmayan sayıların toplam eleman sayısı ve ortalaması
+           Console.WriteLine("\nAsal olmayan sayıların toplam eleman sayısı : "+notWandList.Count);
+           int notWandTotal = 0;
+           double notWandAverage = 0;
+           for (int i = 0; i < notWandList.Count; i++)
+           {
+                notWandTotal += Convert.ToInt32(notWandList[i]);
+                notWandAverage = (double)notWandTotal/(notWandList.Count);
+           }
+           Console.WriteLine("Asal olmayan sayıların ortalama sayısı : "+ notWandAverage);
+           
+
+
+
+        }
+    }
+}
