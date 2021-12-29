73566719547b391dd9088345228a1e559b05563a
ayhan 2855 <Ayhan28.ak@outlook.com.tr>
2021-12-29 10:26:32 +0300

    homework end

---
+++ CSharp_Projeleri/karakterTerstenYazma/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace karakterTerstenYazma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("İfade giriniz :");
+            var expression = Console.ReadLine();
+          Console.WriteLine(ReverseString(expression));
+
+        }
+
+        private static string ReverseString(string value)
+        {
+            var expression = value.Split(" ");
+            var firstLetter = "";
+            var lastExpression = "";
+            var result = "";
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                firstLetter = expression[i].Substring(0, 1);
+                lastExpression = expression[i].Remove(0, 1);
+                result += string.Concat(lastExpression + firstLetter + " ");
+                
+            }
+
+            return result;
+        }
+    }
+}
