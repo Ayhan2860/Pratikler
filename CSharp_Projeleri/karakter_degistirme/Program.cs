1ac44e8559b395d46523763c4d4afcebc711d2db
ayhan 2855 <Ayhan28.ak@outlook.com.tr>
2022-02-01 23:16:26 +0300

    homework end

--- CSharp_Projeleri/karakter_degistirme/Program.cs
+++ CSharp_Projeleri/karakter_degistirme/Program.cs
@@ -8,12 +8,18 @@ namespace karakter_degistirme
         {
             string expiression = "Merhaba Hello Algoritma x";
             string result = "";
+
+            // İfadeyi boşluklardan bölerek string array de saklamak
             string[] expiressionArray = expiression.Split(' ');
+           
+            // array'in uzunluğu kadar gezerek her bir kelimenin ilk karakteri ile son karakterin yerini değiştirmek
             for (var i = 0; i < expiressionArray.Length; i++)
             {
                 int index = expiressionArray[i].Length-1;
+
                 string firstCharacter = expiressionArray[i].Substring(0,1);
                 string lastCharacter = expiressionArray[i].Substring(index);
+
                 result += expiressionArray[i].Remove(0,1).Insert(0,lastCharacter)
                 .Remove(index,1).Insert(index,firstCharacter + " ");
                 
