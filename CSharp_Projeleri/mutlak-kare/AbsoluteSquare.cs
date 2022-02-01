ab14a460da7ba123894629703581dac6c39363bc
ayhan 2855 <Ayhan28.ak@outlook.com.tr>
2022-02-01 21:25:31 +0300

    homework end

--- CSharp_Projeleri/mutlak-kare/AbsoluteSquare.cs
+++ CSharp_Projeleri/mutlak-kare/AbsoluteSquare.cs
@@ -5,28 +5,41 @@ namespace mutlak_kare
 {
     public class AbsoluteSquare
     {
-        public static void Squaring(List<int> arr)
+
+       
+        /// <summary>
+        /// Belirlenmiş bir sayının kendinden küçük olan sayıların farklarının toplamını ve
+        /// kendinden büyük olan sayıların farklarının mutlak karesini toplamını hesaplar
+        ///</summary>
+        /// <param name="arr">Sayıların listesi</param>
+        /// <param name="s">Belirlenmiş sayı</param>
+        public static void Squaring(List<int> arr, int s)
         {
             int totalSum = 0;
             int sumTotal =0;
 
             for (int i = 0; i < arr.Count; i++)
             {
-                if(arr[i] < 67)
+                if(arr[i] < s)
                 {
-                  totalSum += 67 - arr[i]; 
+                  totalSum += Math.Abs(s - arr[i]); 
                 }
 
-                else if(arr[i]> 67)
+                else if(arr[i]> s)
                 {
                    
-                    sumTotal += FindTheSquare(arr[i]-67);
+                    sumTotal += FindTheSquare(arr[i]-s);
                 }
             }
 
-            Console.Write($"\n\n{totalSum, 3}  -  {sumTotal}");
+            Console.Write($"\n\n{totalSum, 3}  {sumTotal}");
         }
-
+         /// <summary>
+        /// Belirlenmiş bir sayının kendinden küçük olan sayıların farklarının toplamını ve
+        /// kendinden büyük olan sayıların farklarının mutlak karesini toplamını hesaplar
+        ///</summary>
+        /// <param name="a">Belirlenmiş sayı</param>
+        /// <returns>Sayının mutlak karesi</returns>
         private static int FindTheSquare(int a)
         {
             return a * a;
--- CSharp_Projeleri/mutlak-kare/Program.cs
+++ CSharp_Projeleri/mutlak-kare/Program.cs
@@ -8,11 +8,22 @@ namespace mutlak_kare
     {
         static void Main(string[] args)
         {
-            Console.Write("Sayıları boşluk bırakarak giriniz : ");
+            
+            Console.Write("Lütfen Hesaplama yapacağınız sayıyı giriniz :");
+            int s = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nSayıları boşluk bırakarak giriniz : ");
+
+           
             List<int>  arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
-             AbsoluteSquare.Squaring(arr);
+             AbsoluteSquare.Squaring(arr,s);
             Console.ReadLine();
         }
+
+        /*
+        Projede kullanıcı tarafında girilen bir sayı dizisinin 67'den küçük yada büyük olduğuna baktıktan sonra; 
+        Küçük olanların farklarının toplamını 
+        büyük ise de farkların mutlak karelerini alarak toplayıp ekrana yazdırdığımız bir Console uygulamamızdır.
+        */
 
        
     }
