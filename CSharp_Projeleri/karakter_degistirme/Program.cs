e434f8bf6f2b170ebeb8996cbd3bbed085c34a3a
ayhan 2855 <Ayhan28.ak@outlook.com.tr>
2022-02-01 23:10:11 +0300

    homework end

---
+++ CSharp_Projeleri/karakter_degistirme/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace karakter_degistirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expiression = "Merhaba Hello Algoritma x";
+            string result = "";
+            string[] expiressionArray = expiression.Split(' ');
+            for (var i = 0; i < expiressionArray.Length; i++)
+            {
+                string firstCharacter = expiressionArray[i].Substring(0,1);
+                string lastCharacter = expiressionArray[i].Substring(expiressionArray[i].Length-1);
+                result += expiressionArray[i].Remove(0,1).Insert(0,lastCharacter)
+                .Remove(expiressionArray[i].Length-1,1).Insert(expiressionArray[i].Length-1,firstCharacter + " ");
+                
+               
+            }
+             result = result.Substring(0,result.Length-1);
+             Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
