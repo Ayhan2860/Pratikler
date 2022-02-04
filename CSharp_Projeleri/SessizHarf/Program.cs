f2b1a098fc5b12ac97c4b0d8a6cea22a92e8dfad
ayhan 2855 <Ayhan28.ak@outlook.com.tr>
2022-02-05 00:41:46 +0300

    homework end

---
+++ CSharp_Projeleri/SessizHarf/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SessizHarf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.Write("İfade giriniz : ");
+            string expiression = "umut";
+             int m = 0;
+            int r = 0;
+            Regex rgx = new Regex("[aeıioöuüAEIİUÜOÖ]");
+            
+            for (int i = 0; i < expiression.Length; i++)
+            {  
+                  var t = expiression[i].ToString();
+                  var g = rgx.Match(t);
+                 
+                  if(!g.Success)
+                  {
+                    r++;
+                    if(r>=3) m=0; else if(r>1) m =r;
+                  }
+                  else r=0;
+            }
+            if(m == 2)
+             Console.WriteLine(true);
+             else  
+             Console.WriteLine(false);
+            
+          
+            Console.ReadKey();
+        }
+    }
+}
