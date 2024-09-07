e8176c6fe103b0a7a3cb97417a31738ed6d4fe5a
ayhan-karaman <ayhan28ak@gmail.com>
2024-09-07 23:52:36 +0300

    Change 3 array list

--- CSharp-101/ArrayList/Program.cs
+++ CSharp-101/ArrayList/Program.cs
@@ -5,8 +5,8 @@ using System.Collections;
 
 ArrayList list = new ArrayList();
 
-// list.Add("Ayşe");
-// list.Add(2);
+list.Add("Ayşe");
+list.Add(2);
 // list.Add(true);
 // list.Add('A');
 
--- CSharp-101/VariablesAndDataTypes/obj/Debug/net8.0/VariablesAndDataTypes.AssemblyInfo.cs
+++ CSharp-101/VariablesAndDataTypes/obj/Debug/net8.0/VariablesAndDataTypes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VariablesAndDataTypes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaf84de87b766fa4da55c63ca61a17f8556eac96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46990ed5a0c538d0647dac800554d087fa87feb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("VariablesAndDataTypes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VariablesAndDataTypes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
