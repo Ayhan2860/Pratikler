ad4a2d4ab2171d0e40c3a9512854263a30599d84
ayhan-karaman <ayhan28ak@gmail.com>
2024-09-07 15:33:01 +0300

    CSharp-101: Operator, decision structures, error handling, type conversions and switch case structure courses completed

---
+++ CSharp-101/TryCatchFinallyAndLogicalFallacies/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+try
+{
+    Console.WriteLine("Bir sayı giriniz..");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Girdiğinin Sayı : {num}");
+
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine($"Hata : {ex.Message.ToString()}");
+}
+finally
+{
+    Console.WriteLine("İşlem Tamamlandı");
+}
+
+try
+{
+    int c = int.Parse("-20000000000");
+    //int a = int.Parse(null);
+    //int b = int.Parse("test");
+
+
+}
+catch (ArgumentNullException ex)
+{
+
+    Console.WriteLine($"\nArgüman null hatası :\n {ex}");
+}
+catch (FormatException ex)
+{
+    Console.WriteLine($"\nFormat hatası :\n {ex}");
+
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine($"\nOver flow hatası :\n {ex}");
+}
+
+
+Console.ReadKey();
---
+++ CSharp-101/TypeConversions/Program.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("***** Implicit Conversion *****");
+
+byte a = 5;
+sbyte b = 30;
+short c = 10;
+
+
+int d = a + b + c;
+Console.WriteLine($"d: {d}");
+
+long h = d;
+Console.WriteLine($"h: {h}");
+
+float i = h;
+Console.WriteLine($"i: {i}");
+
+
+string e = "Ayhan";
+char f = 'K';
+object g = e + f + d;
+
+Console.WriteLine($"g: {g}");
+
+// Explicit Conversion (Bilinçli Dönüşüm)
+Console.WriteLine("\n***** Explicit Conversion *****");
+
+int x = 4;
+byte y = (byte)x;
+Console.WriteLine($"y: {y,5}");
+
+int z = 100;
+byte t = (byte)z;
+Console.WriteLine($"t: {t,5}");
+
+
+float w = 10.3f;
+byte v = (byte)w;
+Console.WriteLine($"v: {v,5}");
+
+//  ToString Methodu
+Console.WriteLine("\n***** ToString Methodu *****");
+
+int xx = 6;
+string yy = xx.ToString();
+Console.WriteLine($"yy: {yy,5}");
+
+string zz = 12.5f.ToString();
+Console.WriteLine($"zz: {zz,5}");
+
+//System.Convert
+Console.WriteLine("\n***** System.Convert Sınıfı *****");
+string s1 = "10", s2 = "20";
+int sayi1, sayi2, toplam;
+
+sayi1 = Convert.ToInt32(s1);
+sayi2 = Convert.ToInt32(s2);
+
+toplam = sayi1 + sayi2;
+Console.WriteLine($"Toplam: {toplam,5}");
+
+
+// Parse
+Console.WriteLine("\n***** System.Convert Sınıfı *****");
+ParseMethod();
+
+
+
+static void ParseMethod()
+{
+    string metin1 = "10";
+    string metin2 = "10.25";
+    int rakam1;
+    double double1;
+
+    rakam1 = Int32.Parse(metin1);
+    double1 = Double.Parse(metin2);
+    Console.WriteLine($"Rakam : {rakam1}");
+    Console.WriteLine($"Double : {double1}");
+
+}
