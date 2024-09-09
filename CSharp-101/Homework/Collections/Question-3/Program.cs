c9105c9e703ed22a4912baa668eca4780d761855
ayhan-karaman <ayhan28ak@gmail.com>
2024-09-09 22:50:31 +0300

    Homework-2 question-3 completed

--- CSharp-101/Homework/Collections/Question-3/Program.cs
+++ CSharp-101/Homework/Collections/Question-3/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.Write("Lütfen bir cümle girniz: ");
+string? sentence = Console.ReadLine();
+char[] character = {'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü', 'A', 'E', 'I', 'İ', 'O', 'Ö', 'U', 'Ü'};
+
+List<char> result = new List<char>();
+Console.WriteLine();
+foreach (var item in sentence!)
+{
+    int index = character.ToList().IndexOf(item);
+    if(index > -1)
+      result.Add(character[index]);
+}
+result.Sort();
+result.ForEach(x => Console.WriteLine(x));
+
